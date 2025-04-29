03e61a392e9d418ec373bf14988afd810af1c717
Marino Michael <marino.michael.1990@gmail.com>
2025-04-29 18:40:37 +0200

    fix url header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,18 @@
-import React from "react";
-import Navbar from "../Navbar/Navbar";
-import "./Header.scss";
-import imgLogo from "../../assets/test.svg";
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import './Header.scss';
+import imgLogo from '../../assets/test.svg';
 
 export default function Header() {
   return (
     <header>
       <div className="navContainer"></div>
       <div className="container">
-        <a href="https://mamprod.com" className="logo">
+        <a
+          href="https://mm-cv-portfolio.netlify.app/"
+          className="logo"
+          target="_self"
+        >
           <img src={imgLogo} alt="logo" />
         </a>
         <Navbar />
